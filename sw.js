e16c820f19dea828e94a412fba41b231e6e9f552
chrscmpl <chcampolongo1@gmail.com>
2024-04-03 11:27:22 +0200

    minor changes

--- sw.js
+++ sw.js
@@ -50,7 +50,6 @@ for (const asset of assets) {
   }
 }
 
-/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
@@ -59,7 +58,6 @@ self.addEventListener('install', function (e) {
   );
 });
 
-/* Serve cached content when offline */
 self.addEventListener('fetch', function (e) {
   e.respondWith(
     caches.match(e.request).then(function (response) {
