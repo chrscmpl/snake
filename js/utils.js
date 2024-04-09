13b6f7d4b9669e6b00dd82229016106978f80d6f
chrscmpl <chcampolongo1@gmail.com>
2024-04-09 12:25:39 +0200

    Minor fix

--- js/utils.js
+++ js/utils.js
@@ -279,7 +279,7 @@ export const MathUtils = (function () {
     randomDifferentThan(a, b, x) {
       if ((x && (x < a || x > b || Math.floor(x) !== x)) || a >= b)
         throw new Error('randomDifferentThan(): invalid inputs');
-      if (b - a === 1) return x === a ? b : a;
+      if (b - a === 1) return x == a ? b : a;
       return _randomDifferentThan(a, b, x);
     },
   };
